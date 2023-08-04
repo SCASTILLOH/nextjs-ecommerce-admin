dea799e08ea934c8d018e2921a8c59db32cebf5c
SCASTILLOH <sebastian.castillo.83z@gmail.com>
2023-08-04 12:46:43 -0400

    crea modal store

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,15 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
-export default function Home() {
-  return (
-    <div className="p-4">
-      <Button size={"default"} variant={"secondary"}>
-        Click me
-      </Button>
-    </div>
-  );
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
+export default function SetupPage() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) onOpen();
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root page</div>;
 }

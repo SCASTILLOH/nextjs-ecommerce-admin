9f1c7c63eeddf964e94c5e50c54590844f602e92
SCASTILLOH <sebastian.castillo.83z@gmail.com>
2023-08-04 20:17:25 -0400

    incluye API route en settings store

--- components/ui/api-alert.tsx
+++ components/ui/api-alert.tsx
@@ -1,9 +1,10 @@
+"use client";
 import { Copy, Server } from "lucide-react";
-
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import toast from "react-hot-toast";
+
 import { Badge, BadgeProps } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import toast from "react-hot-toast";
 
 interface ApiAlertProps {
   title: string;

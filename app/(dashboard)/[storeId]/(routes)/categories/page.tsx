fc54b72c03381c9b343257929f098d4dc198de5c
SCASTILLOH <sebastian.castillo.83z@gmail.com>
2023-08-05 21:48:37 -0400

    implementa categories

---
+++ app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -0,0 +1,35 @@
+import { format } from "date-fns";
+import prismadb from "@/lib/prismadb";
+import BillboardClient from "./components/client";
+import { CategoryColumn } from "./components/columns";
+
+const CategoriesPage = async ({ params }: { params: { storeId: string } }) => {
+  const categories = await prismadb.category.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      billboard: true,
+    },
+    orderBy: {
+      cratedAt: "desc",
+    },
+  });
+
+  const formattedCategories: CategoryColumn[] = categories.map((item) => ({
+    id: item.id,
+    name: item.name,
+    billboardLabel: item.billboard.label,
+    createdAt: format(item.cratedAt, "MMMM do, yyyy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardClient data={formattedCategories} />
+      </div>
+    </div>
+  );
+};
+
+export default CategoriesPage;

6f8c663db8b91901d66a61b0bed27ad63516550b
SCASTILLOH <sebastian.castillo.83z@gmail.com>
2023-08-06 16:57:39 -0400

    impementa order page

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,24 @@
+"use client";
+import Heading from "@/components/heading";
+import { Separator } from "@/components/ui/separator";
+import { OrderColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+
+interface OrderClientProps {
+  data: OrderColumn[];
+}
+
+const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
+  return (
+    <>
+      <Heading
+        title={`Orders (${data.length})`}
+        description="Manage orders for your store"
+      />
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey={"products"} />
+    </>
+  );
+};
+
+export default OrderClient;

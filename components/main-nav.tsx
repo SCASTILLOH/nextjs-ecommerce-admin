e5e606246a59d1f2a19e63ca9630a1e343daa37f
SCASTILLOH <sebastian.castillo.83z@gmail.com>
2023-08-06 12:44:39 -0400

    avanza con product page form

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -39,6 +39,11 @@ const MainNav = ({
       active: pathname === `/${params.storeId}/colors`,
     },
     {
+      href: `/${params.storeId}/products`,
+      label: "Product",
+      active: pathname === `/${params.storeId}/products`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `/${params.storeId}/settings`,
